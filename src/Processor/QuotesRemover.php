8fa7abb994de3f9591344aa728fb1fae5f9c4459
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2023-06-04 18:42:04 +0300

    deprecated QuotesRemover processor

--- src/Processor/QuotesRemover.php
+++ src/Processor/QuotesRemover.php
@@ -4,21 +4,25 @@ namespace gulch\Minify\Processor;
 
 use gulch\Minify\Contract\ProcessorInterface;
 
+/**
+ * @deprecated deprecated since version 1.1
+ */
 class QuotesRemover extends Replacer implements ProcessorInterface
 {
     public function getReplacePatternData(): array
     {
         return [
-            '/src="(.*?)"/' => 'src=$1',
-            '/width="(.*?)"/' => 'width=$1',
-            '/height="(.*?)"/' => 'height=$1',
-            '/name="(.*?)"/' => 'name=$1',
-            '/charset="(.*?)"/' => 'charset=$1',
-            '/align="(.*?)"/' => 'align=$1',
-            '/border="(.*?)"/' => 'border=$1',
-            '/crossorigin="(.*?)"/' => 'crossorigin=$1',
-            '/type="(.*?)"/' => 'type=$1',
             '/\/>/' => '>',
+            '/align="([^\s]*?)"/' => 'align=$1',
+            '/border="([^\s]*?)"/' => 'border=$1',
+            '/charset="([^\s]*?)"/' => 'charset=$1',
+            /* '/content="([^\s]*?)"/' => 'content=$1', */
+            '/crossorigin="([^\s]*?)"/' => 'crossorigin=$1',
+            '/height="([^\s]*?)"/' => 'height=$1',
+            '/name="([^\s]*?)"/' => 'name=$1',
+            '/src="([^\s]*?)"/' => 'src=$1',
+            '/type="([^\s]*?)"/' => 'type=$1',
+            '/width="([^\s]*?)"/' => 'width=$1',
         ];
     }
 }
