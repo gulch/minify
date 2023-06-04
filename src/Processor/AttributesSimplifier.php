f08575442b04bd7de6f1d5df4f7706c87cb61780
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2023-06-04 18:40:16 +0300

    enhanced list of attributes for AttributesSimplifier Processor

--- src/Processor/AttributesSimplifier.php
+++ src/Processor/AttributesSimplifier.php
@@ -9,12 +9,27 @@ class AttributesSimplifier extends Replacer implements ProcessorInterface
     public function getReplacePatternData(): array
     {
         return [
-            '/ method=("get"|get)/i' => '',
-            '/ disabled=[^ >]*(.*?)/' => ' disabled',
-            '/ readonly=[^ >]*(.*?)/' => ' readonly',
-            '/ selected=[^ >]*(.*?)/' => ' selected',
+            '/ allowfullscreen=[^ >]*(.*?)/' => ' allowfullscreen',
             '/ async=[^ >]*(.*?)/' => ' async',
+            '/ autofocus=[^ >]*(.*?)/' => ' autofocus',
+            '/ autoplay=[^ >]*(.*?)/' => ' autoplay',
+            '/ checked=[^ >]*(.*?)/' => ' checked',
+            '/ controls=[^ >]*(.*?)/' => ' controls',
             '/ defer=[^ >]*(.*?)/' => ' defer',
+            '/ disabled=[^ >]*(.*?)/' => ' disabled',
+            '/ formnovalidate=[^ >]*(.*?)/' => ' formnovalidate',
+            '/ itemscope=[^ >]*(.*?)/' => ' itemscope',
+            '/ loop=[^ >]*(.*?)/' => ' loop',
+            '/ method=("get"|get)/i' => '',
+            '/ multiple=[^ >]*(.*?)/' => ' multiple',
+            '/ muted=[^ >]*(.*?)/' => ' muted',
+            '/ nomodule=[^ >]*(.*?)/' => ' nomodule',
+            '/ novalidate=[^ >]*(.*?)/' => ' novalidate',
+            '/ open=[^ >]*(.*?)/' => ' open',
+            '/ open=[^ >]*(.*?)/' => ' open',
+            '/ readonly=[^ >]*(.*?)/' => ' readonly',
+            '/ required=[^ >]*(.*?)/' => ' required',
+            '/ selected=[^ >]*(.*?)/' => ' selected',
         ];
     }
 }
